16124fe8ffd96d153aa90ea22ddf5e92f747349c
SalahEstwani <eng.salah.estwani@hotmail.com>
2024-03-04 02:32:40 +0100

    additional

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
 import {data} from "./project.js"
 let container = document.querySelector(".container")
 function pokemonCard() {
-    let content = ""
-    for(let i=0; i< data.length; i++){
-    content+=`<div class="card">
-    <p class="pokemon-id">${data[i].id}</p>
-    <p class="pokemon-name">${data[i].name}</p>
-    <img class="pokemon-thumbnail" src="${data[i].thumbnail}"/>
-    <p class="pokemon-type">${data[i].type}</p>
-    </div>`
+    let content = "";
+    for (let i = 0; i < data.length; i++) {
+        content += `<div class="card">
+            <div class="id-container">
+                <p class="pokemon-id">${data[i].id}</p>
+            </div>
+            <div class="name-container">
+                <p class="pokemon-name">${data[i].name}</p>
+            </div>
+                <img class="pokemon-thumbnail" src="${data[i].thumbnail}" />
+            <div class="type-container">
+                <p class="pokemon-type">${data[i].type}</p>
+            </div>
+        </div>`;
+    }
+    container.innerHTML = content;
+}
 
-}
-container.innerHTML=content
-}
 pokemonCard()
+
+
+function searchPokemon() {
+    let searchInput = document.getElementById("searchInput").value.toLowerCase();
+    let filteredData = data.filter(pokemon => pokemon.type.toLowerCase().includes(searchInput));
+    pokemonCard(filteredData);
+}
